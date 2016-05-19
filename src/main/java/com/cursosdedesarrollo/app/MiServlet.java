a81fbde8b83718a955d12cf8555627dbacc40741
pepesan <pepesan@gmail.com>
2016-05-19 12:57:34 +0200

    creado el primer servlet

---
+++ src/main/java/com/cursosdedesarrollo/app/MiServlet.java
@@ -0,0 +1,26 @@
+package com.cursosdedesarrollo.app;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by pepesan on 19/5/16.
+ */
+public class MiServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws IOException, ServletException{
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<h1>Hola Mundo desde Servlet!</h1>");
+        out.println("<a href='/'>Inicio</a>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
